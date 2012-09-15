f07f7b56e8b280ab95e029ef3226084896d155b4
Tomasz Pewiński <pewniak747@gmail.com>
2012-09-15 17:29:15 +0200

    converted specs to minitest

--- spec/deep_fetch_spec.rb
+++ spec/deep_fetch_spec.rb
@@ -1,53 +1,53 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
 require_relative '../lib/deep_fetch'
-require 'rspec'
 
 describe Hash do
   describe "deep_fetch" do
-    it "should return value from hash" do
-      {:foo => :bar}.deep_fetch(:foo).should == :bar
+    it "must_equal return value from hash" do
+      {:foo => :bar}.deep_fetch(:foo).must_equal :bar
     end
 
-    it "should return value from block if key not found" do
-      {:foo => :bar}.deep_fetch(:baz) { :boo } .should == :boo
+    it "must_equal return value from block if key not found" do
+      {:foo => :bar}.deep_fetch(:baz) { :boo } .must_equal :boo
     end
 
-    it "should rise KeyError if no block and key not found" do
-      expect do
-        {:foo => :bar}.deep_fetch(:baz).should
-      end.to raise_error(KeyError)
+    it "must_equal rise KeyError if no block and key not found" do
+      lambda do
+        {:foo => :bar}.deep_fetch(:baz).must_equal
+      end.must_raise(KeyError)
     end
 
-    it "should fetch from deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar).should == :baz
+    it "must_equal fetch from deep hash" do
+      {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar).must_equal :baz
     end
 
-    it "should return value from block if key not found in deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz) { :boo } .should == :boo
+    it "must_equal return value from block if key not found in deep hash" do
+      {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz) { :boo } .must_equal :boo
     end
 
-    it "should raise KeyError if key not found in deep hash and no block" do
-      expect do
+    it "must_equal raise KeyError if key not found in deep hash and no block" do
+      lambda do
         {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz)
-      end.to raise_error(KeyError)
+      end.must_raise(KeyError)
     end
 
-    it "should return values from arrays in deep hash" do
-      {:foo => [nil, {:bar => :baz}]}.deep_fetch(:foo, 1, :bar).should == :baz
+    it "must_equal return values from arrays in deep hash" do
+      {:foo => [nil, {:bar => :baz}]}.deep_fetch(:foo, 1, :bar).must_equal :baz
     end
 
-    it "should fetch hash from deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo).should == {:bar => :baz}
+    it "must_equal fetch hash from deep hash" do
+      {:foo => {:bar => :baz}}.deep_fetch(:foo).must_equal({:bar => :baz})
     end
 
-    it "should fetch array from deep hash" do
-      {:foo => {:bar => [:baz]}}.deep_fetch(:foo, :bar).should == [:baz]
+    it "must_equal fetch array from deep hash" do
+      {:foo => {:bar => [:baz]}}.deep_fetch(:foo, :bar).must_equal [:baz]
     end
 
-    it "should not evaluate a default block when fetching from deep hash" do
+    it "must_equal not evaluate a default block when fetching from deep hash" do
       bomb = ->() { raise "BOOM" }
-      expect do
-        {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar, &bomb).should == :baz
-      end.not_to raise_error
+      {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar, &bomb).must_equal :baz
     end
   end
 end
