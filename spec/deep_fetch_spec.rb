cc6b1f37096adde788caba69ecc9760c6008c046
Tomasz Pewiński <pewniak747@gmail.com>
2013-01-25 21:30:11 +0100

    refactor specs

--- spec/deep_fetch_spec.rb
+++ spec/deep_fetch_spec.rb
@@ -5,65 +5,71 @@ require_relative '../lib/deep_fetch'
 
 describe Hash do
   describe "deep_fetch" do
-    it "must_equal return value from hash" do
-      {:foo => :bar}.deep_fetch(:foo).must_equal :bar
+    describe "must return value" do
+      it "from shallow hash" do
+        {:foo => :bar}.deep_fetch(:foo).must_equal :bar
+      end
+
+      it "from block if key was not found" do
+        {:foo => :bar}.deep_fetch(:baz) { :boo } .must_equal :boo
+      end
+
+      it "from deep hash" do
+        {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar).must_equal :baz
+      end
+
+      it "from block if key not found in deep hash" do
+        {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz) { :boo } .must_equal :boo
+      end
+
+      it "from arrays in deep hash" do
+        {:foo => [nil, {:bar => :baz}]}.deep_fetch(:foo, 1, :bar).must_equal :baz
+      end
+
+      it "of hash from deep hash" do
+        {:foo => {:bar => :baz}}.deep_fetch(:foo).must_equal({:bar => :baz})
+      end
+
+      it "of array from deep hash" do
+        {:foo => {:bar => [:baz]}}.deep_fetch(:foo, :bar).must_equal [:baz]
+      end
+
+      it "of hash from deeper hash" do
+        {:foo => {:bar => {:baz => :boo}}}.deep_fetch(:foo, :bar).must_equal({:baz => :boo})
+      end
     end
 
-    it "must_equal return value from block if key not found" do
-      {:foo => :bar}.deep_fetch(:baz) { :boo } .must_equal :boo
+    describe "must rise KeyError" do
+      it "if key not found in hash and no block provided" do
+        lambda do
+          {:foo => :bar}.deep_fetch(:baz).must_equal
+        end.must_raise(KeyError)
+      end
+
+      it "if key not found in deep hash and no block provided" do
+        lambda do
+          {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz)
+        end.must_raise(KeyError)
+      end
+
+      it "must raise KeyError when deep key does not exist" do
+        lambda do
+          {:foo => :bar}.deep_fetch(:foo, :foo)
+        end.must_raise(KeyError)
+      end
     end
 
-    it "must_equal rise KeyError if no block and key not found" do
-      lambda do
-        {:foo => :bar}.deep_fetch(:baz).must_equal
-      end.must_raise(KeyError)
+    describe "must rise ArgumentError" do
+      it "if invoked without arguments" do
+        lambda do
+          {}.deep_fetch
+        end.must_raise(ArgumentError)
+      end
     end
 
-    it "must_equal fetch from deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar).must_equal :baz
-    end
-
-    it "must_equal return value from block if key not found in deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz) { :boo } .must_equal :boo
-    end
-
-    it "must_equal raise KeyError if key not found in deep hash and no block" do
-      lambda do
-        {:foo => {:bar => :baz}}.deep_fetch(:foo, :baz)
-      end.must_raise(KeyError)
-    end
-
-    it "must_equal return values from arrays in deep hash" do
-      {:foo => [nil, {:bar => :baz}]}.deep_fetch(:foo, 1, :bar).must_equal :baz
-    end
-
-    it "must_equal fetch hash from deep hash" do
-      {:foo => {:bar => :baz}}.deep_fetch(:foo).must_equal({:bar => :baz})
-    end
-
-    it "must_equal fetch array from deep hash" do
-      {:foo => {:bar => [:baz]}}.deep_fetch(:foo, :bar).must_equal [:baz]
-    end
-
-    it "must_equal fetch hash from deeper hash" do
-      {:foo => {:bar => {:baz => :boo}}}.deep_fetch(:foo, :bar).must_equal({:baz => :boo})
-    end
-
-    it "must_equal not evaluate a default block when fetching from deep hash" do
+    it "must not evaluate a default block when fetching from deep hash" do
       bomb = ->() { raise "BOOM" }
       {:foo => {:bar => :baz}}.deep_fetch(:foo, :bar, &bomb).must_equal :baz
-    end
-
-    it "must raise ArgumentError if invoked without arguments" do
-      lambda do
-        {}.deep_fetch
-      end.must_raise(ArgumentError)
-    end
-
-    it "must raise KeyError when deep key does not exist" do
-      lambda do
-        {:foo => :bar}.deep_fetch(:foo, :foo)
-      end.must_raise(KeyError)
     end
   end
 end
