1bafd855124383a18b4b219f4bf502e49ae58cf9
Tomasz Pewiński <pewniak747@gmail.com>
2013-07-19 17:21:32 +0200

    refactoring

--- lib/deep_fetch/core_ext.rb
+++ lib/deep_fetch/core_ext.rb
@@ -3,17 +3,19 @@ class Hash
     raise ArgumentError.new("wrong number of arguments (0 for 1..n)") if args.empty?
 
     value = fetch(args.shift, &block)
-    if value.kind_of?(Hash) && args.size > 0
-      value.deep_fetch(*args, &block)
-    elsif value.kind_of?(Array) && args.size > 0
-      value = value[args.shift]
-      if args.size > 0
+    if args.size > 0
+      if value.kind_of?(Hash)
         value.deep_fetch(*args, &block)
+      elsif value.kind_of?(Array)
+        value = value[args.shift]
+        if args.size > 0
+          value.deep_fetch(*args, &block)
+        else
+          value
+        end
       else
-        value
+        {}.fetch(args.shift, &block)
       end
-    elsif args.size > 0
-      {}.fetch(args.shift, &block)
     else
       value
     end
